6e063b32d419519dc3644207da9f5268f605ed93
JesseYoung <jesse0830@vip.qq.com>
2018-09-13 11:31:33 +0800

    前端react框架搭建完成

---
+++ jl_app/src/pages/index.jsx
@@ -0,0 +1,17 @@
+//首页
+'use strict'
+
+import React,{ Component } from 'react'
+import ReactDOM from 'react-dom';
+export default class Index extends Component {
+    constructor(props){
+        super(props);
+    }
+    state = {
+    }
+    render() {
+        return (
+            <div>JesseYoung</div>
+        )
+    }
+}
