e1597711554b8e65d6d1d5c3c06369cb77f162fe
jesseyoung <jesse0830@vip.qq.com>
2018-11-14 09:15:13 +0800

    JesseYoung: 代码提交1114

--- jl_app/src/pages/profile/ProfileIndex.jsx
+++ jl_app/src/pages/profile/ProfileIndex.jsx
@@ -1,15 +1,41 @@
 import React, {Component} from 'react'
+import {Button, InputItem, WhiteSpace} from 'antd-mobile'
+import {Icon} from 'antd'
+import './ProfileIndex.css'
 export default class ProfileIndex extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
     }
 
-    componentDidMount(){
+    componentDidMount() {
 
     }
-    render(){
-        return(
-            <div>用户设置</div>
+
+    render() {
+        return (
+            <div className="login-page" style={{height:document.documentElement.clientHeight - 85}}>
+                <div className="input-main">
+                    <InputItem
+                        className="user-input"
+                        placeholder="请输入用户名"
+                        clear={true}
+                        name="userNameInput"/>
+                    <WhiteSpace/>
+                    <InputItem
+                        className="pwd-input"
+                        type="password"
+                        placeholder="请输入密码"
+                        clear={true}
+                        name="passwordInput"/>
+                    <WhiteSpace/>
+                    <Button
+                        type="primary"
+                        size="small"
+                        className="login-button">
+                        登陆
+                    </Button>
+                </div>
+            </div>
         )
     }
 }
