fd0c4889df8d89002304ee5c670135ffbef79ab8
qianlimawww <qianlimawww@sina.com>
2018-11-22 23:32:27 +0800

    modify ProfileIndex.jsx

--- jl_app/src/pages/profile/ProfileIndex.jsx
+++ jl_app/src/pages/profile/ProfileIndex.jsx
@@ -1,16 +1,22 @@
 import React, {Component} from 'react'
-import {Button, InputItem, WhiteSpace,Toast} from 'antd-mobile'
+import {Button, InputItem, WhiteSpace,Toast,Modal} from 'antd-mobile'
 import {Icon} from 'antd'
 import './ProfileIndex.css'
 import ajaxGetData from '../../ajaxGetData'
+import register from "../../registerServiceWorker";
 
 let userName = ""
 let passWord = ""
+
+let registerName = ""
+let registerPwd = ""
+
 export default class ProfileIndex extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            login : false
+            login : false,
+            registerModal : false,
         }
     }
 
@@ -47,6 +53,16 @@ export default class ProfileIndex extends Component {
         console.log("click logout")
     }
 
+    register = () => {
+        console.log("click register button")
+
+        this.setState({
+            registerModal : true
+        })
+
+    }
+
+
     userNameOnchange = (e) => {
         userName = e
     }
@@ -54,6 +70,50 @@ export default class ProfileIndex extends Component {
     passWordOnchange = (e) => {
         passWord = e
     }
+
+    registerUserNameOnchange = (e) => {
+        registerName = e
+    }
+
+    registerPassWordOnchange = (e) => {
+        registerPwd = e
+    }
+
+    onClose = (modal) => {
+        this.setState({
+            registerModal : modal
+        })
+        console.log("cancel register")
+    }
+
+    //注册函数
+    onRegister () {
+        // const self = this
+        // ajaxGetData({
+            // url: '/api/server/register',
+            // data: {"username":userName,"password":passWord},
+            // success: function(result) {
+            //     console.log(result)
+            //     if (result) {
+            //         Toast.info("登陆成功",2)
+            //         clearTimeout(setTimeoutId)
+            //         let setTimeoutId = setTimeout(()=>{
+            //             self.setState({
+            //          username = registerName
+        // passWord = ""
+        // registerPwd = ""
+        // registerName = ""
+            //                 registerModal : false
+            //             })
+            //         },2000)
+            //     }
+            //     else{
+            //         Toast.info("注册失败",2)
+            //     }
+            // }
+        // })
+    }
+
     render() {
         return (
             <div className="login-page" style={{height: document.documentElement.clientHeight - 85}}>
@@ -80,7 +140,46 @@ export default class ProfileIndex extends Component {
                         onClick={this.login}>
                         登陆
                     </Button>
+                    <WhiteSpace/>
+                    <Button
+                        type="ghost"
+                        size="small"
+                        className="register-button"
+                        onClick={this.register}>
+                        注册
+                    </Button>
                 </div>
+                <Modal className="modal-css"
+                    visible={this.state.registerModal}
+                       transparent
+                       maskClosable={true}
+                       title="注册"
+                       footer={[{ text: 'Ok', onPress: () => { console.log('ok'); this.onRegister(); } },{ text: 'Cancel', onPress: () => { console.log('cancel'); this.onClose(false); } }]}
+                >
+                    <div className="register-main" >
+                        <InputItem
+                            className="register-user-input"
+                            placeholder="请输入注册用户名"
+                            clear={true}
+                            name="registerUserNameInput"
+                            onChange={this.registerUserNameOnchange}/>
+
+
+                        <span className="tips">
+                            建议使用手机号注册
+                        </span>
+
+
+
+                        <InputItem
+                            className="register-pwd-input"
+                            placeholder="请输入注册密码"
+                            clear={true}
+                            name="registerPwdNameInput"
+                            onChange={this.registerPassWordOnchange}/>
+                        <WhiteSpace/>
+                    </div>
+                </Modal>
             </div>
         )
     }
